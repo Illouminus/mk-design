ceba56bc2c81fb70c64a44b5409ac039e492c99c
Illouminus <professionnel.baillot@gmail.com>
2023-05-23 17:42:57 +0200

    adding theme switcher, ui kit, svgr loader, file loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,23 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from "react-router-dom";
-import {MainPage} from "../pages/MainPage";
-import {AboutPage} from "../pages/AboutPage";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import './styles/index.scss'
+import {AppRouter} from "./providers/router";
+import {Navbar} from "widgets/Navbar";
+
 
 
 
 export const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading</div>}>
-                <Routes>
-                    <Route path={'/'} element={<MainPage /> }/>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                </Routes>
-            </Suspense>
+
+
+            <Navbar />
+            <AppRouter />
+
+
         </div>
     );
 };
