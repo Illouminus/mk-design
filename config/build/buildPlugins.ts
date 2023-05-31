a95d978e046200eadec4d44cff64169ef3f3ebc4
Illouminus <professionnel.baillot@gmail.com>
2023-05-31 17:34:58 +0200

    another fix of dev plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,7 +5,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new HTMLWebpackPlugin({
       template: paths.html
     }),
@@ -16,12 +16,15 @@ export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPl
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
-    }),
-    isDev
-      ? new BundleAnalyzerPlugin({
+    })
+  ]
+  if (isDev) {
+    plugins.push(
+      new BundleAnalyzerPlugin({
         openAnalyzer: false
       })
-      : undefined
+    )
+  }
 
-  ]
+  return plugins
 }
